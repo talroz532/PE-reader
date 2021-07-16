0d9a772266e14f6352a3b0440f28184b97bf89fc
talroz532 <67608539+talroz532@users.noreply.github.com>
2021-07-16 15:36:16 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -4,35 +4,3 @@ IMAGE_DOS_HEADER DosHeader;
 IMAGE_NT_HEADERS NTHeader;
 IMAGE_FILE_HEADER FileHeader;
 IMAGE_OPTIONAL_HEADER OptinalHeader;
-/*
-Magic
-MajorLinkerVersion
-MinorLinkerVersion
-SizeOfCode
-SizeOfInitializedData
-SizeOfUninitializedData
-AddressOfEntryPoint
-BaseOfCode;
-BaseOfData;
-ImageBase
-SectionAlignment
-FileAlignment
-MajorOperatingSystemVersion
-MinorOperatingSystemVersion
-MajorImageVersion
-MinorImageVersion
-MajorSubsystemVersion
-MinorSubsystemVersion
-Win32VersionValue
-SizeOfImage
-SizeOfHeaders
-CheckSum
-Subsystem
-DllCharacteristics
-SizeOfStackReserve
-SizeOfStackCommit
-SizeOfHeapReserve
-SizeOfHeapCommit
-LoaderFlags
-NumberOfRvaAndSizes
-*/
