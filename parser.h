767ff071f92c5ff08da61c7ea405aee57bc52256
talroz532 <67608539+talroz532@users.noreply.github.com>
2021-07-16 15:35:24 +0300

    Add files via upload

---
+++ parser.h
@@ -0,0 +1,38 @@
+
+//declareing
+IMAGE_DOS_HEADER DosHeader;
+IMAGE_NT_HEADERS NTHeader;
+IMAGE_FILE_HEADER FileHeader;
+IMAGE_OPTIONAL_HEADER OptinalHeader;
+/*
+Magic
+MajorLinkerVersion
+MinorLinkerVersion
+SizeOfCode
+SizeOfInitializedData
+SizeOfUninitializedData
+AddressOfEntryPoint
+BaseOfCode;
+BaseOfData;
+ImageBase
+SectionAlignment
+FileAlignment
+MajorOperatingSystemVersion
+MinorOperatingSystemVersion
+MajorImageVersion
+MinorImageVersion
+MajorSubsystemVersion
+MinorSubsystemVersion
+Win32VersionValue
+SizeOfImage
+SizeOfHeaders
+CheckSum
+Subsystem
+DllCharacteristics
+SizeOfStackReserve
+SizeOfStackCommit
+SizeOfHeapReserve
+SizeOfHeapCommit
+LoaderFlags
+NumberOfRvaAndSizes
+*/
